d52953eadd945b0e7193fceac0dd9bbdf621ad3d
kimbro97 <rlagudwog@gmail.com>
2024-01-24 00:54:55 +0900

    feat: 다형성-역할과 구현 예제2

--- src/poly/car0/Driver.java
+++ src/poly/car0/Driver.java
@@ -2,14 +2,26 @@ package poly.car0;
 
 public class Driver {
     private K3Car k3Car;
+    private Model3Car model3Car;
 
     public void setK3Car(K3Car k3Car) {
         this.k3Car = k3Car;
     }
 
+    public void setModel3Car(Model3Car model3Car) {
+        this.model3Car = model3Car;
+    }
+
     public void drive() {
-        this.k3Car.startEngine();
-        this.k3Car.pressAccelerator();
-        this.k3Car.offEngine();
+        if (k3Car != null) {
+            this.k3Car.startEngine();
+            this.k3Car.pressAccelerator();
+            this.k3Car.offEngine();
+        }
+        if (model3Car != null) {
+            this.model3Car.startEngine();
+            this.model3Car.pressAccelerator();
+            this.model3Car.offEngine();
+        }
     }
 }
--- src/poly/car0/K3Car.java
+++ src/poly/car0/K3Car.java
@@ -2,14 +2,14 @@ package poly.car0;
 
 public class K3Car {
     public void startEngine() {
-        System.out.println("시동 on");
+        System.out.println("K3Car시동 on");
     }
 
     public void offEngine() {
-        System.out.println("시동 off");
+        System.out.println("K3Car시동 off");
     }
 
     public void pressAccelerator() {
-        System.out.println("악셀");
+        System.out.println("K3Car악셀");
     }
 }

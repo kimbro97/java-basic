a2e01ed00faeb2e41b1bac4ca2b747b07f61e432
kimbro97 <rlagudwog@gmail.com>
2024-01-23 23:20:50 +0900

    feat: 추상 클래스1

---
+++ src/poly/ex3/AbstractMain.java
@@ -0,0 +1,19 @@
+package poly.ex3;
+
+public class AbstractMain {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+        Duck duck = new Duck();
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(caw);
+        soundAnimal(duck);
+    }
+
+    private static void soundAnimal(AbstractAnimal abstractAnimal) {
+        abstractAnimal.sound();
+    }
+}
---
+++ src/poly/ex3/Pig.java
@@ -0,0 +1,8 @@
+package poly.ex3;
+
+public class Pig extends AbstractAnimal {
+    @Override
+    public void sound() {
+        System.out.println("꿀꿀");
+    }
+}
